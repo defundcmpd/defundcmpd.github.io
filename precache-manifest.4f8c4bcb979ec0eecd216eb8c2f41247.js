f7505af6108f26e42ea06f7aebe4f4a9261ff7ef
defundcmpd <defundCharMeckPD@gmail.com>
2020-06-04 12:24:08 -0400

    Updates

---
+++ precache-manifest.4f8c4bcb979ec0eecd216eb8c2f41247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb648da3f7f7beaec2125e9e58209ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a17583ec7710f175088e",
+    "url": "/static/css/main.528018cf.chunk.css"
+  },
+  {
+    "revision": "b3840d90acc7da4f256d",
+    "url": "/static/js/2.93849d33.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.93849d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17583ec7710f175088e",
+    "url": "/static/js/main.41ab53ed.chunk.js"
+  },
+  {
+    "revision": "5384ab1369897d6571d5",
+    "url": "/static/js/runtime-main.422284ed.js"
+  }
+]);
